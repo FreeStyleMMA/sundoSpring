d21e720763822fe2fe86207b058987792d7dad7f
jy <sjy0574@gmail.com>
2025-10-15 15:10:33 +0900

    201015

--- src/main/java/com/sundostudio/service/MemberServiceImpl.java
+++ src/main/java/com/sundostudio/service/MemberServiceImpl.java
@@ -1,14 +1,6 @@
 package com.sundostudio.service;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +9,6 @@ import com.sundostudio.dto.LoginRequest;
 import com.sundostudio.dto.LoginResponse;
 import com.sundostudio.dto.MemberVO;
 import com.sundostudio.mapper.MemberMapper;
-import com.sundostudio.security.CustomUserDetails;
 import com.sundostudio.security.JwtUtil;
 
 import lombok.Setter;
